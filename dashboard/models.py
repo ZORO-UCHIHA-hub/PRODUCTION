59b5c92ad2388438d5a8e3b90a51799fbc729fd4
ZORO-UCHIHA-hub <swordsman.debajyoti@gmail.com>
2025-07-10 21:45:34 +0530

    Update models.py

--- dashboard/models.py
+++ dashboard/models.py
@@ -24,7 +24,7 @@ class Profile(models.Model):
     phone = models.CharField(max_length=15, unique=True, null=True, blank=True)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
     branch = models.ForeignKey(Branch, null=True, blank=True, on_delete=models.SET_NULL)
-    gst_number = models.CharField(max_length=15, null=True, blank=True)
+    gst_number = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return f"{self.user.username} ({self.role})"
@@ -60,7 +60,7 @@ class Customer(models.Model):
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=15)
     branch = models.ForeignKey(Branch, on_delete=models.CASCADE, null=True, blank=True)
-    gst_number = models.CharField(max_length=15, null=True, blank=True)
+    gst_number = models.CharField(max_length=100, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
